cf4ffc9f9f6207ae40f2f1f1361cb3083c81a3e9
Brandon R. Stoner <monokrome@monokro.me>
2011-06-26 02:29:52 +0000

    Renamed project "django-edict"

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
-try:
-    from setuptools import setup, find_packages
-    from setuptools.command.test import test
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
-    from setuptools.command.test import test
+#!/usr/bin/env python
+from distutils.core import setup
+import os
 
-setup(name = 'django-news',
+parent_directory = os.path.abspath(os.path.dirname(__file__))
+
+meta_files = {
+    'README.md': None,
+    'CLASSIFIERS.txt': None,
+}
+
+for filename in meta_files:
+    try:
+        current_file = open(os.path.join(parent_directory, filename))
+        meta_files[filename] = current_file.read()
+        current_file.close()
+    except IOError:
+        raise IOError('{0} not found.'.format(filename))
+
+classifiers = meta_files['CLASSIFIERS.txt'].split('\n')
+classifiers.remove('')
+
+setup(name='django-edict',
+      version='0.1',
       description = 'A django application for keeping your fans up to date.',
-      author = 'Brandon R. Stoner',
-      author_email = 'monokrome@monokro.me',
-      version = '0.7',
-
-      zip_safe = False,
-      include_package_data = True,
-      packages = find_packages(),
-      url = 'http://github.com/monokrome/django-news/',
-
-      classifiers = [
-          "Development Status :: 4 - Beta",
-          "Environment :: Web Environment",
-          "Intended Audience :: Developers",
-          "Operating System :: OS Independant",
-          "Programming Language :: Python",
-          "Framework :: Django",
-          "Topic :: Internet :: WWW/HTTP / :: Dynamic Content",
-          "Topic :: Internet :: WWW/HTTP / :: Site Management",
-      ],
+      long_description=meta_files['README.md'],
+      classifiers=classifiers,
+      author='Brandon R. Stoner',
+      author_email='monokrome@limpidtech.com',
+      url='http://github.com/LimpidTech/django-edict',
+      packages=['edict'],
+      keywords = 'web django blog news edict',
 )
 
